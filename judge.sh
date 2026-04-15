a54bb1d7a3cbce62900a210269ae76b7a0c55398
Kohei Nishiwaki <128705560+hduehgw0@users.noreply.github.com>
2026-04-15 14:22:59 +0900

    refactor: シェル変数をダブルクォートで囲むベストプラクティスを適用

--- judge.sh
+++ judge.sh
@@ -12,7 +12,7 @@ ruby "$PROGRAM" < "$INPUT" > "$ACTUAL" 2> /dev/null
 # 正常終了 -> 0  異常終了 -> 0以外
 RB_STATUS=$?
 
-if [ $RB_STATUS -ne 0 ]; then
+if [ "$RB_STATUS" -ne 0 ]; then
     echo "RE"
     exit 1
 fi
@@ -22,7 +22,7 @@ diff -q "$EXPECTED" "$ACTUAL" > /dev/null
 # 完全一致　-> 0  差異あり -> 1
 DIFF_STATUS=$?
 
-if [ $DIFF_STATUS -eq 0 ]; then
+if [ "$DIFF_STATUS" -eq 0 ]; then
     echo "AC"
 else
     echo "WA"
